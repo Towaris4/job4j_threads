17ae557a5b047ca672b1666404afb81bbd64828e
Towaris4 <ivan.tarasov2488@gmail.com>
2026-04-08 20:03:20 +0300

    4. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,22 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        int index = 0;
+                        while (index <= 100) {
+                            System.out.print("\rLoading : " + index + "%");
+                            Thread.sleep(100);
+                            index++;
+                        }
+                        System.out.println("\nLoaded.");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+    }
+}
