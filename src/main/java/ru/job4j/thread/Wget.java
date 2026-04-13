0342b4ce211fffb0cbb4b6e823e8a91810fede29
Towaris4 <ivan.tarasov2488@gmail.com>
2026-04-13 12:41:40 +0300

    8. Скачивание файла с ограничением. [#144271 #562123]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -5,10 +5,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.Thread.sleep;
 
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private static final Pattern FILENAME_PATTERN = Pattern.compile(".*/([^/]+)$");
 
     public Wget(String url, int speed) {
         this.url = url;
@@ -17,32 +22,63 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
+        int loadBytes = 0;
         var startAt = System.currentTimeMillis();
-        var file = new File("tmp.xml");
+        String fileName = null;
+        Matcher matcher = FILENAME_PATTERN.matcher(url);
+        if (matcher.find()) {
+            fileName = matcher.group(1);
+        }
+        String filePath = "src/main/resources/" + fileName;
+        var file = new File(filePath);
         String url = this.url;
         try (var input = new URL(url).openStream();
              var output = new FileOutputStream(file)) {
             System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
             var dataBuffer = new byte[512];
             int bytesRead;
+            long sleepTime;
+            var downloadAt = System.nanoTime();
+            int downloadBytes = 0;
+            long realSpeed;
             while ((bytesRead = input.read(dataBuffer, 0, dataBuffer.length)) != -1) {
-                var downloadAt = System.nanoTime();
+                downloadBytes += bytesRead;
+                realSpeed = (long) downloadBytes * 1_000_000_000L / (System.nanoTime() - downloadAt);
+                if (realSpeed > speed) {
+                    sleepTime = realSpeed / speed;
+                    sleep(sleepTime);
+                }
                 output.write(dataBuffer, 0, bytesRead);
-                System.out.println("Read 512 bytes : " + (System.nanoTime() - downloadAt) + " nano.");
             }
             System.out.println(Files.size(file.toPath()) + " bytes");
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        String url = args[0];
+        Pattern URL_PATTERN = Pattern.compile(
+                "^(https?:\\/\\/)?([\\w\\-\\.]+)\\.([a-z]{2,6}\\.?)(\\/[\\w\\-\\.\\/]*)*\\/?$");
+        if (!(args.length == 2)) {
+            System.out.println("Отсутствует необходимое количество параметров.");
+        }
+        String url = null;
+        int speed = 0;
+        for (String arg : args) {
+            if (arg != null && URL_PATTERN.matcher(arg).matches()) {
+                url = arg;
+            }
+        }
         if (url.isEmpty()) {
             System.out.println("Отсутствует ссылка.");
         }
-        int speed = Integer.parseInt(args[1]);
-        if (args[1].isEmpty() || (speed == 0)) {
+        Pattern NUMBER_PATTERN = Pattern.compile("^[-+]?\\d+([.,\\s]\\d+)?$");
+        for (String arg : args) {
+            if (arg != null && NUMBER_PATTERN.matcher(arg).matches()) {
+                speed = Integer.parseInt(arg);
+            }
+        }
+        if (speed == 0) {
             System.out.println("Неверное ограничение по скорости.");
         }
         Thread wget = new Thread(new Wget(url, speed));
