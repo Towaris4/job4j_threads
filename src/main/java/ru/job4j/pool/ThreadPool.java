b4df4e48e1ccfc87e6cb395d0dce58b8005b07b4
Towaris4 <ivan.tarasov2488@gmail.com>
2026-04-24 20:19:17 +0300

    1. Реализовать ThreadPool [#1099 #562840]

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -9,17 +9,16 @@ public class ThreadPool {
     private final List<Thread> threads = new LinkedList<>();
     private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(100);
     private final int size = Runtime.getRuntime().availableProcessors();
-    private volatile boolean run = true;
 
     public ThreadPool() throws InterruptedException {
             for (int i = 1; i <= size; i++) {
                 Thread thread = new Thread(() -> {
                     try {
-                        while (run) {
+                        while (!Thread.currentThread().isInterrupted()) {
                             tasks.poll().run();
                         }
                     } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
+                        Thread.currentThread().interrupt();
                     }
                 });
                 threads.add(thread);
@@ -36,7 +35,6 @@ public class ThreadPool {
     }
 
     public void shutdown() {
-        run = false;
         for (Thread thread : threads) {
             try {
                 thread.interrupt();
