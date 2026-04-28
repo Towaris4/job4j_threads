cd80d714a9ffca18d891e7adb94d90970274b7a2
Towaris4 <ivan.tarasov2488@gmail.com>
2026-04-28 20:45:17 +0300

    4. CompletableFuture [#361626 #563070]

--- src/main/java/ru/job4j/pools/RolColSum.java
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -7,26 +7,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class RolColSum {
-    public static class Sums {
-        private int rowSum;
-        private int colSum;
-
-        public int getRowSum() {
-            return rowSum;
-        }
-
-        public void setRowSum(int rowSum) {
-            this.rowSum = rowSum;
-        }
-
-        public int getColSum() {
-            return colSum;
-        }
-
-        public void setColSum(int colSum) {
-            this.colSum = colSum;
-        }
-    }
 
     public static Sums[] sum(int[][] matrix) {
         Sums[] sums = new Sums[matrix.length];
